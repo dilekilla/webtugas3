eb4524f267454eead7b4e2e9a7b1757a56b0f6d1
dilekilla <dildilpa00@gmail.com>
2024-01-23 17:17:58 +0700

    chore: init repo

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        display: ["Abril Fatface", "serif"],
+        sans: ["Lato", "sans-serif" ],
+      },
+    },
+  },
+  plugins: [],
+};
